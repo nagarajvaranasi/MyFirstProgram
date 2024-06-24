61c14214e4f0fa2bc85853d616b4ac4e51a7b0a3
Varanasi Nagaraj <Varanasi.Nagaraj@covetrus.com>
2024-06-24 13:29:42 +0530

    Add project files.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,51 @@
+﻿using MyFirstProgram;
+
+Student student = new Student("123456789");
+
+//student.Name = "Pavan";
+//student.Dob = "01-01-2000";
+//student.Age = 30;
+////student.PhoneNumber="1234567890";
+
+
+//Console.WriteLine(student.Name);
+
+//Console.WriteLine(student.Dob);
+
+//Console.WriteLine(student.Age);
+
+//student.ShowPhoneNumber();
+
+Console.WriteLine("Enter Student Name");
+student.Name = Console.ReadLine();
+
+Console.WriteLine("Enter Student Dob");
+student.Dob = Console.ReadLine();
+
+
+Console.ReadLine();
+
+Console.WriteLine("Do you want to enter correct DOB : Yes/No");
+
+string result = Console.ReadLine();
+
+if(result == "Yes")
+{
+    Console.WriteLine("Enter Student Dob again");
+    student.Dob = Console.ReadLine();
+    Console.WriteLine("Student Details");
+    Console.WriteLine(student.Name);
+    Console.WriteLine(student.Dob);
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Student Details");
+    Console.WriteLine(student.Name);
+    Console.WriteLine(student.Dob);
+    Console.ReadLine();
+
+}
+
+
+
---
+++ MyFirstProgram/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    public class Student
+    {
+       public Student(string phnNumber) { 
+
+            PhoneNumber = phnNumber;
+        }
+
+        public string Id;
+
+        public string Name;
+
+        public int Age;
+
+        public string Dob;
+
+        public char Sex;
+
+        private string PhoneNumber;
+
+        string CountryCode { get; set; }
+
+        public void ShowPhoneNumber()
+        {
+            Console.WriteLine(PhoneNumber);
+        }
+    }
+}
